de20d4d2466ae0620836ea4f10f805610e86df2e
Facundo Castellano <castellanofacundo05@gmail.com>
2025-11-15 18:32:44 -0300

    Set up train/test split and fit Decision Tree classifier

--- car-evaluation.py
+++ car-evaluation.py
@@ -27,6 +27,14 @@ X = pd.get_dummies(X)
 
 #print(X.head())
 
+# Dividir en entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=14142135)
+
+print("Datos listos...")
+print("Entrenando el modelo...")
+model = DecisionTreeClassifier(random_state=14142135)
+model.fit(X_train, y_train)
+
 
 
 
